c8a2315d6d321c0b73512006f1f0602f7e7b1014
diegothomas2025 <diegothomas4@hotmail.com>
2025-07-15 18:36:09 -0300

    W02 Learning Activity: Handling DOM Events Completed

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,12 +2,31 @@ const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
 
-li.textContent = input.value;
-deleteButton.textContent = "❌";
 
-li.append(deleteButton);
-list.append(li);
+button.addEventListener("click", function () {
+    
+    if (input.value.trim() !== "") {
 
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
+
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
+
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener("click", function () {
+            li.remove();
+        });
+
+        input.value = "";
+        input.focus();
+        
+    } else {
+        alert("Plase write an answer")
+        input.focus();
+    }
+
+});
