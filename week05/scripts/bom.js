5c4bbb0687f31b5cac05baf7082954b60cf75840
diegothomas2025 <diegothomas4@hotmail.com>
2025-10-12 20:13:34 -0300

    W05 Learning Activity: JavaScript localStorage Completed

---
+++ week05/scripts/bom.js
@@ -0,0 +1,50 @@
+let input = document.querySelector("#favchap");
+let button = document.querySelector("button");
+let list = document.querySelector("#list");
+
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+button.addEventListener("click", () => {
+    if (input.value != "") {
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
+        input.value = "";
+        input.focus();
+    }
+});
+
+function displayList(item) {
+    let li = document.createElement("li");
+    let deleteButton = document.createElement('button');
+    li.textContent = item;
+    deleteButton.textContent = "❌";
+    deleteButton.classList.add('delete');
+    li.append(deleteButton);
+    list.append(li);
+
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        deleteChapter(li.textContent);
+        input.focus();
+    });
+    console.log('I like to copy code instead of typing it out myself and trying to understand it.');
+};
+
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+};
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+};
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+};
